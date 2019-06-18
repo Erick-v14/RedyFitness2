45fd455058868d872d7ff9a357fa702b1f129b17
Erick Valle <erick.t.valle@outlook.com>
2019-06-18 13:15:48 -0400

    Add project files.

---
+++ RedyFitness2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedyFitness2.Startup))]
+namespace RedyFitness2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
